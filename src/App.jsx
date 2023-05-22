19e71e306d397b051f903bc375d3099d991e51d0
NexoCreative-MDE <nexo-creative@outlook.com>
2023-05-22 18:44:15 -0500

    fortuneCookies v1.0

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+
+import { useState } from 'react'
+import './App.css'
+import ButtonPhrase from './components/ButtonPhrase'
+import PhrasesCard from './components/PhrasesCard'
+import phrases from './utils/phrases.json'
+import randomElementFromArray from './utils/randomElementFromArray'
+import CopyRight from './components/copyRight'
+
+const arrBackground = [1, 2, 3, 4, 5, 6, 7, 8]
+
+function App() {
+  
+  const initialPhrase = randomElementFromArray(phrases)
+  const initialPath =  randomElementFromArray(arrBackground)
+  
+  const [phraseRandom, setPhraseRandom] = useState(initialPhrase)
+  const [pathRandom, setPathRandom] = useState(initialPath)
+  
+  const objStyle = {
+    backgroundImage: `url('./fondo${pathRandom}.jpg'), url('./fondo${pathRandom}.png')`    
+  }
+    
+  return (
+   <div style={objStyle} className='app'>
+   <h1 className='app__title'>Galleta de la fortuna</h1>
+   <ButtonPhrase 
+    setPhraseRandom={setPhraseRandom} 
+    setPathRandom={setPathRandom}
+    arrBackground={arrBackground}
+    />
+   <PhrasesCard  phraseRandom={phraseRandom}/>
+   <CopyRight />
+   </div>
+  )
+}
+
+export default App
---
+++ src/components/copyRight.jsx
@@ -0,0 +1,10 @@
+
+const CopyRight = () => {
+  return (
+    <div className="copyright">
+        <span>Powered By Leonardo Diaz Castrillon - ceise@live.com  <a href="https://github.com/NexoCreative-MDE/fortuneCookies.git">https://github.com/NexoCreative-MDE/fortuneCookies.git</a></span>
+    </div>
+  )
+}
+
+export default CopyRight
